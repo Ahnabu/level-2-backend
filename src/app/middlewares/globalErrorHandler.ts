e45dbba05f89cb67a1067a3754ccf3a44784b7e4
Abu Horaira <syedmdabuhoraira@gmail.com>
2025-04-22 00:11:12 +0600

    updated some error handling functions

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,20 +1,46 @@
 
-/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import express, { Application, Request, Response,NextFunction } from 'express'
+import express, { Application, Request, Response,NextFunction, ErrorRequestHandler } from 'express'
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorSource } from '../interface/error';
+import handleZodError from '../errors/handleZodErrors';
+import handleValidationError from '../errors/handleValidationError';
 
 const app:Application = express()
 
 
-const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-    const statusCode = err.status || 500;
-    const errorMessage = err.message || 'Internal Server Error';
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next)=> {
+  let statusCode = err.status || 500;
+  let message = err.message || 'Internal Server Error';
+ 
+  let errorSources: TErrorSource = [{
+    path: '',
+    message: "something went wrong"
+  }]
+
   
-    res.status(statusCode).json({
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  
+      
+  } else if (err.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  }
+res.status(statusCode).json({
       success: false,
-      message: errorMessage,
+      message: message,
       stack: process.env.NODE_ENV === 'production' ? undefined : err.stack,
+      error: errorSources,
     });
 }
+
+
   
 export default globalErrorHandler;
