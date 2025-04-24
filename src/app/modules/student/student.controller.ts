4bf6f5faa75a49098fa579e9a4bb0f1eda11abc9
Abu Horaira <syedmdabuhoraira@gmail.com>
2025-04-24 21:37:01 +0600

    code refactored

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,78 +1,60 @@
-import {  Request, RequestHandler, Response } from 'express';
+import { RequestHandler } from 'express';
 import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import { StudentServices } from './student.service';
-import catchAsync from '../../utils/catchAsync';
 
-const getSingleStudent = catchAsync (async (
-  req: Request,
-  res: Response,
+const getSingleStudent = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await StudentServices.getSingleStudentFromDB(id);
 
-) => {
-  const { studentId } = req.params;
-    const result = await StudentServices.getSingleStudentFromDB(studentId);
-
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student is retrieved successfully',
-      data: result,
-    });
-});
-const getAllStudents:RequestHandler = catchAsync(async (
-  req: Request,
-  res: Response,
-
-) => {
-
-    const result = await StudentServices.getAllStudentsFromDB(req.query);
-
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student are retrieved successfully',
-      data: result,
-    });
- 
-
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student is retrieved successfully',
+    data: result,
+  });
 });
 
-const deleteStudent = catchAsync(async (
-  req: Request,
-  res: Response,
+const getAllStudents: RequestHandler = catchAsync(async (req, res) => {
+  const result = await StudentServices.getAllStudentsFromDB(req.query);
 
-) => {
-
-    const { studentId } = req.params;
-    const result = await StudentServices.deleteStudentFromDB(studentId);
-
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student is deleted successfully',
-      data: result,
-    });
- 
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student are retrieved successfully',
+    data: result,
+  });
 });
 
+const updateStudent = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const { student } = req.body;
+  const result = await StudentServices.updateStudentIntoDB(id, student);
 
-const updateAStudent = catchAsync(async (req: Request, res: Response) => {
-      const { studentId } = req.params;
-      const { student: studentData } = req.body;
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student is updated successfully',
+    data: result,
+  });
+});
 
-      const result = await StudentServices.updateAStudent(studentId, studentData);
+const deleteStudent = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await StudentServices.deleteStudentFromDB(id);
 
-      res.status(200).json({
-          success: true,
-          message: 'Student is updated successfully',
-          data: result,
-      });
- })
-
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student is deleted successfully',
+    data: result,
+  });
+});
 
 export const StudentControllers = {
   getAllStudents,
   getSingleStudent,
   deleteStudent,
-  updateAStudent,
+  updateStudent,
 };
--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,13 +1,14 @@
+import httpStatus from 'http-status';
 import mongoose from 'mongoose';
+import QueryBuilder from '../../builder/QueryBuilder';
+import AppError from '../../errors/AppError';
+import { User } from '../user/user.model';
+import { studentSearchableFields } from './student.constant';
 import { TStudent } from './student.interface';
 import { Student } from './student.model';
-import AppError from '../../errors/AppError';
-import QueryBuilder from '../../builder/QueryBuilder';
-import { studentSearchableFields } from './student.constant';
 
-const getAllStudentsFromDB = async (query:Record<string,unknown>) => {
-
- /*
+const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
+  /*
   const queryObj = { ...query }; // copying req.query object so that we can mutate the copy object 
    
   let searchTerm = '';   // SET DEFAULT VALUE 
@@ -106,6 +107,7 @@ const getAllStudentsFromDB = async (query:Record<string,unknown>) => {
   return fieldQuery;
 
   */
+
   const studentQuery = new QueryBuilder(
     Student.find()
       .populate('admissionSemester')
@@ -125,88 +127,97 @@ const getAllStudentsFromDB = async (query:Record<string,unknown>) => {
 
   const result = await studentQuery.modelQuery;
   return result;
-
 };
 
 const getSingleStudentFromDB = async (id: string) => {
-  const result = await Student.findById(id).populate("admissionSemester").populate({
-    path: "academicDepartment",
-    populate: "academicFaculty",
-  }).lean();
+  const result = await Student.findById(id)
+    .populate('admissionSemester')
+    .populate({
+      path: 'academicDepartment',
+      populate: {
+        path: 'academicFaculty',
+      },
+    });
+  return result;
+};
+
+const updateStudentIntoDB = async (id: string, payload: Partial<TStudent>) => {
+  const { name, guardian, localGuardian, ...remainingStudentData } = payload;
+
+  const modifiedUpdatedData: Record<string, unknown> = {
+    ...remainingStudentData,
+  };
+
+  
+
+  if (name && Object.keys(name).length) {
+    for (const [key, value] of Object.entries(name)) {
+      modifiedUpdatedData[`name.${key}`] = value;
+    }
+  }
+
+  if (guardian && Object.keys(guardian).length) {
+    for (const [key, value] of Object.entries(guardian)) {
+      modifiedUpdatedData[`guardian.${key}`] = value;
+    }
+  }
+
+  if (localGuardian && Object.keys(localGuardian).length) {
+    for (const [key, value] of Object.entries(localGuardian)) {
+      modifiedUpdatedData[`localGuardian.${key}`] = value;
+    }
+  }
+
+  const result = await Student.findByIdAndUpdate(id, modifiedUpdatedData, {
+    new: true,
+    runValidators: true,
+  });
   return result;
 };
 
 const deleteStudentFromDB = async (id: string) => {
-  const isExist = await Student.findOne({ id });
-  if (!isExist) {
-    throw new AppError(404, 'Student not found');
-  }
   const session = await mongoose.startSession();
+
   try {
     session.startTransaction();
 
-    const result = await Student.findOneAndUpdate({ id }, { isDeleted: true },
-       {new:true, session}
+    const deletedStudent = await Student.findByIdAndUpdate(
+      id,
+      { isDeleted: true },
+      { new: true, session },
     );
-    if (!result) {
-      throw new Error('Failed to delete student');
+
+    if (!deletedStudent) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to delete student');
     }
 
-    const deletedUser = await Student.findOneAndUpdate({ id }, { session }, {new:true,session});
+    // get user _id from deletedStudent
+    const userId = deletedStudent.user;
+
+    const deletedUser = await User.findByIdAndUpdate(
+      userId,
+      { isDeleted: true },
+      { new: true, session },
+    );
+
     if (!deletedUser) {
-      throw new AppError(404,'Failed to delete student');
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to delete user');
     }
 
     await session.commitTransaction();
     await session.endSession();
 
-  return result;
-  }catch{
+    return deletedStudent;
+  } catch (err: string | unknown) {
     await session.abortTransaction();
     await session.endSession();
-    throw new AppError(404,'Failed to delete student');
+    throw new Error(`Failed to delete student, ${err}`);
   }
- 
 };
-
-const updateAStudent = async (id: string, student: Partial<TStudent>) => {
-  
-  const {name,guardian,localGuardian,... remainingStudentData} = student;
-
-  const modifiedStudentData:Record<string,unknown> = {
-...remainingStudentData
-  }
-
-  if (name && Object.keys(name).length > 0) { 
-    for(const[key,value] of Object.entries(name)){
-      modifiedStudentData[`name.${key}`] = value;
-    }
-  }
-  if (guardian && Object.keys(guardian).length > 0) {
-    for (const [key, value] of Object.entries(guardian)) {
-      modifiedStudentData[`guardian.${key}`] = value;
-    }
-  }
-
-  if (localGuardian && Object.keys(localGuardian).length > 0) {
-    for (const [key, value] of Object.entries(localGuardian)) {
-      modifiedStudentData[`localGuardian.${key}`] = value;
-    }
-  }
-  const result = await Student.findOneAndUpdate({ id }, modifiedStudentData, {
-    new: true,
-    runValidators: true,
-  }
-  );
-  return result;
-  
-   
-
-}
 
 export const StudentServices = {
   getAllStudentsFromDB,
   getSingleStudentFromDB,
+  updateStudentIntoDB,
   deleteStudentFromDB,
-  updateAStudent,
 };
