2a9f82cba900d82bc7fe6f06361350ac7504a4dd
Abu Horaira <syedmdabuhoraira@gmail.com>
2025-06-08 11:22:54 +0600

    env updated

--- backend/src/app/config/index.ts
+++ backend/src/app/config/index.ts
@@ -9,14 +9,14 @@ export default {
   bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
   jwt_access_secret: process.env.JWT_ACCESS_SECRET,
   jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
-  NODE_ENV: process.env.NODE_ENV,
+  NODE_ENV: process.env.NODE_ENV || 'production',
   jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
   jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
-  reset_password_url: process.env.RESET_PASSWORD_URL,
+  reset_password_url: process.env.RESET_PASSWORD_URL || "https://ums-backend-one.vercel.app/api/v1/auth/reset-password",
   nodemailer_mail: process.env.NODEMAILER_MAIL,
   nodemailer_pass: process.env.NODEMAILER_PASS,
   reset_password_ui_url:
-    process.env.RESET_PASSWORD_UI_URL ,
+    process.env.RESET_PASSWORD_UI_URL||'https://ums-backend-one.vercel.app/' ,
   cloudinary_cloud_name:
     process.env.CLOUDINARY_CLOUD_NAME,
   cloudinary_api_key: process.env.CLOUDINARY_API_KEY ,
